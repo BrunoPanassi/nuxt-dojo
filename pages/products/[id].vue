881b117e5694c6c5ce76c743573ede53f0d90559
BrunoPanassi <bhenrique95@outlook.com>
2023-12-01 13:04:04 -0300

    add pages and route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Checkout out this id: {{ id }}</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Products</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
